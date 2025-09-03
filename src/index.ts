7f4b40e762f1ac18229698d6cac0acd508df644b
leung <rexingleung@126.com>
2025-09-03 17:20:43 +0800

    导出钱包状态管理和类型定义

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Header } from "./components/Header";
 export { WalletComponent } from "./components/Wallet";
+export { useWalletStore, NETWORKS } from "./stores/walletStore";
+export type { WalletState } from "./stores/walletStore";
